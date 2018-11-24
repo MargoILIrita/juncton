44a389eeb2c42419d407e63455297348dfdda4f3
Margarita <rita96pol@gmail.com>
2018-11-25 01:11:00 +0400

    notification fixed beta upload pic

--- MAIN.py
+++ MAIN.py
@@ -1,24 +1,20 @@
+import os
 import time
 
 import face_recognition
 import cv2
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 import bot_telegram
+
+def fill_arrays():
+    files = os.listdir('img/')
+    print(files)
+
 
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-tommy_Face = face_recognition.load_image_file("images-of-me/img.jpg")
+tommy_Face = face_recognition.load_image_file("img/img.jpg")
 tommy_face_encoding = face_recognition.face_encodings(tommy_Face)[0]
 
 # Load a second sample picture and learn how to recognize it.
--- bot_telegram.py
+++ bot_telegram.py
@@ -1,6 +1,5 @@
-from time import sleep
-
 import requests
+from telegram.ext import Updater, CommandHandler,MessageHandler, Filters
 
 token = '704225691:AAE3-FAZdTpmJAbnfrxWlFxoUsBawuy_bjk'
 url = "https://api.telegram.org/bot"+ token +"/"
@@ -30,14 +29,48 @@ def send_mess(chat, text):
     return response
 
 
-
-
-
 def main():
     user_id = (110541740, 110541740)
     user_name = user_id[0]
     if user_id[0] == user_name:
         send_mess(user_id[1], 'You have a discount!')
 
-if __name__ == '__main__':
-    main()
+__urls = []
+
+token = '704225691:AAE3-FAZdTpmJAbnfrxWlFxoUsBawuy_bjk'
+
+
+def start(bot, update):
+    update.message.reply_text(
+        'Hello {}! Please, send me your selfie as a document'.format(update.message.from_user.first_name))
+
+def upload_img(bot, update):
+    id = 'img/' + update.message.chat.id.__str__() + '__' + update.message.from_user.first_name.__str__()
+    file_id = update.message.document.file_id
+    uuuu = url + 'getFile'
+    response = requests.get(uuuu,data={'file_id':file_id})
+    answer = response.json()
+    urr = 'https://api.telegram.org/file/bot' + token + '/' + answer['result']['file_path']
+    response = requests.get(urr)
+    content = response.content
+    f = open(id +'.jpg','wb', 0)
+    f.write(content)
+    f.flush()
+    f.close()
+    print('all')
+    update.message.reply_text("Thanks! Now go to the camera")
+
+
+
+
+updater = Updater(token)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler('start', start))
+
+image_handler = MessageHandler(Filters.document, upload_img)
+dispatcher.add_handler(image_handler)
+
+updater.start_polling()
+updater.idle()
+
