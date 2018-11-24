4141def967d86f7ccb49c0dfa64d790a3bba96e5
Tomas Ye <t.ye@sap.com>
2018-11-24 23:40:21 +0200

    recognition of face now sends a notification to phone

--- android.py
+++ android.py
@@ -1,27 +1,20 @@
-import time
-
 import face_recognition
 import cv2
-import bot_telegram
+from notifications import send_fb_notification
 
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-tommy_Face = face_recognition.load_image_file("img/img.jpg")
+tommy_Face = face_recognition.load_image_file("unknown-images/img.jpg")
 tommy_face_encoding = face_recognition.face_encodings(tommy_Face)[0]
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("unknown-images/winner.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     tommy_face_encoding,
-    biden_face_encoding
 ]
 known_face_names = [
     "Tommy",
-    "Second Tommy"
 ]
 
 # Initialize some variables
@@ -58,39 +51,12 @@ while True:
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
-                print("Tommy in picture")
                 if isNotificationNeed:
-                    bot_telegram.send_mess(110541740, "Congratulations! You have a discount! You're Tommy")
+                    send_fb_notification()
                     isNotificationNeed = False
-            else: print("Tommy not in picture")
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
-
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
