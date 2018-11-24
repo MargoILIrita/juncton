743ebf8a200f482becee109c11b01a5baf7a76ad
Margarita <rita96pol@gmail.com>
2018-11-24 18:41:59 +0400

    fix too much notification

--- trial.py
+++ trial.py
@@ -1,17 +1,9 @@
+import time
+
 import face_recognition
 import cv2
 import bot_telegram
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
@@ -38,7 +30,13 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
+
+sttime = time.time()
+isNotificationNeed = True
 while True:
+    if (time.time() - sttime) > 600:
+        isNotificationNeed = True
+        sttime = time.time()
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
@@ -65,7 +63,9 @@ while True:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
                 print("Tommy in picture")
-                bot_telegram.send_mess(110541740, "You have a discount!")
+                if isNotificationNeed:
+                    bot_telegram.send_mess(110541740, "Congratulations! You have a discount! You're Tommy")
+                    isNotificationNeed = False
             else: print("Tommy not in picture")
             face_names.append(name)
 
